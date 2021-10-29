5d017d41637187183e71062b767a8a3a01cddf88
Promise Dada <gdadapromise@gmail.com>
2021-10-28 22:58:09 -0400

    settings fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,15 +13,17 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import environ
 
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Take environment variables from .env file
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, True)
 )
-environ.Env.read_env()
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
