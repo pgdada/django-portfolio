83e0fe1ae26409ceace04c2ab881e12235751b33
Promise Dada <gdadapromise@gmail.com>
2021-11-03 21:24:50 -0400

    secure domain update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['promisedada.com', 'www.promisedada.com', 'still-brushlands-21910.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['promisedada.com', 'https://www.promisedada.com', 'still-brushlands-21910.herokuapp.com','localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
