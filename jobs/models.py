75a3ec65f3cc514fdf3332c794e91776e3967211
Promise Dada <gdadapromise@gmail.com>
2022-02-09 18:22:53 -0500

    job details

--- jobs/models.py
+++ jobs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    image = models.ImageField(upload_to='media/images/')
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
     description = models.TextField(blank=True)
 
